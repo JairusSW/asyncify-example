ec129c0ce2a7f0250f924e59a730d3a38869b17d
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-02-05 21:44:09 -0800

    add methods

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,6 +1,13 @@
+import * as asyncify from "bindings/asyncify";
+
 @external("env", "sleep")
 declare function sleep(ms: i32): void;
 
+@external("env", "log")
+declare function log(num: i32): void;
+
 export function main(): void {
+  log(0);
   sleep(2000);
+  log(1);
 }
--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 import { readFileSync } from "fs";
-const binary = readFileSync("./build/release.wasm");
+
+function pause() {
+    if (paused) {
+        wasmExports.asyncify_stop_rewind();
+        paused = false;
+    } else {
+        memory[DATA_ADDR >> 2] = DATA_ADDR + 8;
+        memory[DATA_ADDR + 4 >> 2] = 1024;
+        wasmExports.asyncify_start_unwind(DATA_ADDR);
+        paused = true;
+    }
+}
+
+function resume() {
+    wasmExports.asyncify_start_rewind(DATA_ADDR);
+    wasmExports.main();
+}
+
+const binary = readFileSync("./build/test.wasm");
 const compiled = new WebAssembly.Module(binary);
 const instance = new WebAssembly.Instance(compiled, {
     env: {
         sleep: function (ms) {
-            if (!sleeping) {
-                console.log("Winding... Call stack paused!");
-                view[DATA_ADDR >> 2] = DATA_ADDR + 8;
-                view[DATA_ADDR + 4 >> 2] = 1024;
-                wasmExports.asyncify_start_unwind(DATA_ADDR);
-                sleeping = true;
-                setTimeout(function () {
-                    wasmExports.asyncify_start_rewind(DATA_ADDR);
-                    wasmExports.main();
-                }, ms);
-            } else {
-                console.log('Unwinding... Call stack resumed!');
-                wasmExports.asyncify_stop_rewind();
-                sleeping = false;
-            }
-        }
+            pause();
+            setTimeout(function () {
+                resume();
+            }, ms);
+        },
+        log: function (num) {
+            console.log(num ? "Waking up... Call stack resumed!" : "Sleeping... Call stack paused!");
+        },
+        abort: function () { }
     }
 });
-const wasmExports = instance.exports;
-const view = new Int32Array(wasmExports.memory.buffer);
 
-const DATA_ADDR = 16;
-let sleeping = false;
+const wasmExports = instance.exports;
+const memory = new Int32Array(wasmExports.memory.buffer);
+
+const DATA_ADDR = 8;
+let paused = false;
 
 wasmExports.main();
 wasmExports.asyncify_stop_unwind();
